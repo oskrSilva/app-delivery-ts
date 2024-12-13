8a6780a55e4ccd78f917566532b749b474d2cbf6
oscar <oscar.silva@adsmovil.com>
2024-12-13 15:21:29 -0300

    Refactorizacion del codigo

---
+++ src/data/categoriesData.ts
@@ -0,0 +1,24 @@
+import { Category } from '../interfaces/category';
+
+export const categories: Category[] = [
+  {
+    id: 1,
+    name: 'Ropa',
+    image: 'https://http2.mlstatic.com/D_NQ_NP_892390-MLA74409705175_022024-O.webp',
+  },
+  {
+    id: 2,
+    name: 'Alimentos',
+    image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg814v2ia8wE-oJn_Rb4idN0Ih-SCXZsfOPiQO8ZQ7QnjaPxubcJIAYYrxCxYotcn0HIAAEbcAW1zOk4tCEUuKWUdTEoIpXwiRzkELmLfpu-wa1jvMgfOlA2IL6Y60ImiVC5rHs5sohq5U/s1600/comidas-rapidas-en-cali-ciudad-completa.jpg',
+  },
+  {
+    id: 3,
+    name: 'Librería',
+    image: 'https://http2.mlstatic.com/D_NQ_NP_981561-MLA78799716334_092024-O.webp',
+  },
+  {
+    id: 4,
+    name: 'Helados',
+    image: 'https://cuisinart.com.co/cdn/shop/articles/Helado_de_Fresa_Casero.jpg?v=1720632982&width=700',
+  },
+];
